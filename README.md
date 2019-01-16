d5e881007720d2f7db2f1ea73264effa547f81fb
Li Yan <lyan@aviatrix.com>
2019-01-15 16:30:17 -0800

    Initial script commit and README udpate.

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # EBS-encryption
 This repo shows how to encrypt an AWS EBS root volume. You can use the script to apply to Aviatrix Controller and gateways. 
+
+Requirements
+------------
+-       Install AWS Python SDK [Boto3](https://github.com/boto/boto3#quick-start) 
+-       Install AWS CLI [AWSCLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html)
+-       AWS CLI Configuration [Configure your client](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html)
+
+-       IAM Permissions
+        These IAM policy actions are required in order to run this script. This is an example policy you can attach to a user to give them the proper IAM authorization.
+	    {
+                "Version": "2010-10-10",
+		"Statement": [
+		    {
+		        "Effect": "Allow",
+	                "Action": [
+		            "ec2:DescribeInstances",
+			    "ec2:DescribeVolumes",
+			    "ec2:DescribeSnapshots",
+			    "ec2:StopInstances",
+			    "ec2:StartInstances",
+			    "ec2:CopySnapshot",
+			    "ec2:CreateSnapshot",
+			    "ec2:CreateVolume",
+			    "ec2:DeleteVolume",
+			    "ec2:DeleteSnapshot",
+			    "ec2:AttachVolume",
+			    "ec2:DetachVolume"
+		        ],
+		        "Resource": "*"
+		    }
+		]
+	    } 
+
+
+Execute the script to encrypt root volume
+-----------------------------------------
+syntax: enc_volume.py [-h] -i INSTANCE_ID [-key CUSTOMER_MASTER_KEY]
+                     [-region REGION] [-p PROFILE] [-key_id AWS_ACCESS_KEY_ID]
+                     [-secret_key AWS_SECRET_ACCESS_KEY]
+
+Example 1:
+To encrypt root volume for instance i-04b8deaeb555fbee8 with default AWS CLI created profile userx and AWS default key management system:
+```sh
+sudo ./enc_volume.py -i i-04b8deaeb555fbee8 -p userx
+```
+
+Example 2:
+To encrypt root volume for instance i-04b8deaeb555fbee8 with default AWS CLI created profile userx and customer master key:
+
+```sh
+sudo ./enc_volume.py -i i-04b8deaeb555fbee8 -p userx -key xxx
+```
+
+Example 3:
+To encrypt root volume for instance i-04b8deaeb555fbee8 in region us-east-1 with AWS Access Key ID yyy and Secret Access Key zzz and customer master key xxx:
+
+```sh
+sudo ./enc_volume.py -i i-04b8deaeb555fbee8 --key xxx --region us-east-1 --aws_access_key_id yyy --aws_secret_access_key zzz
+```
---
+++ enc_volume.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+ 
+"""
+Overview:
+Take unencrypted root volume and encrypt it for EC2.
+Params:
+ID for EC2 instance
+Customer Master Key (CMK) (optional)
+Profile to use (optional)
+AWS Access Key ID (optional)
+AWS Secret Access Key (optional)
+Conditions:
+Return if volume already encrypted
+Use named profiles from credentials file
+"""
+
+import sys
+import boto3
+import botocore
+import argparse
+ 
+def main(argv):
+    parser = argparse.ArgumentParser(description='Encrypts EC2 root volume.')
+    parser.add_argument('-i', '--instance_id)',
+        help='Instance to encrypt volume on.', required=True)
+    parser.add_argument('-key', '--customer_master_key',
+        help='Customer master key', required=False)
+    parser.add_argument('-region', '--region',
+        help='AWS region', required=False)
+    parser.add_argument('-p', '--profile',
+        help='Profile to use', required=False)
+    parser.add_argument('-key_id', '--aws_access_key_id',
+        help='AWS Access Key ID to use', required=False)
+    parser.add_argument('-secret_key', '--aws_secret_access_key',
+        help='AWS Secret Access Key to use', required=False)
+    args = parser.parse_args()
+
+    """ Set up AWS Session + Client + Resources + Waiters """
+    if args.profile:
+        # Create custom session
+        print('Using profile {}'.format(args.profile))
+        session = boto3.session.Session(profile_name=args.profile)
+    else:
+        # Use default session
+        session = boto3.session.Session(region_name=args.region,
+                                        aws_access_key_id=args.aws_access_key_id,
+                                        aws_secret_access_key=args.aws_secret_access_key)
+        #aws_session_token=SESSION_TOKEN,
+        #sts.get_session_token()
+
+    client = session.client('ec2')
+    ec2 = session.resource('ec2')
+ 
+    waiter_instance_exists = client.get_waiter('instance_exists')
+    waiter_instance_stopped = client.get_waiter('instance_stopped')
+    waiter_instance_running = client.get_waiter('instance_running')
+    waiter_snapshot_complete = client.get_waiter('snapshot_completed')
+    waiter_volume_available = client.get_waiter('volume_available')
+
+    # Get CMK
+    customer_master_key = args.customer_master_key
+  
+    """ Check instance exists """
+    instance_id = args.instance_id
+    print('---Checking instance ({})'.format(instance_id))
+    instance = ec2.Instance(instance_id)
+ 
+    try:
+        waiter_instance_exists.wait(
+            InstanceIds=[
+                instance_id,
+            ]
+        )
+    except botocore.exceptions.WaiterError as e:
+        sys.exit('ERROR: {}'.format(e))
+
+    """ Get volume and exit if already encrypted """
+    volumes = [v for v in instance.volumes.all()]
+    if volumes:
+        original_root_volume = volumes[0]
+        volume_encrypted = original_root_volume.encrypted
+        if volume_encrypted:
+            sys.exit(
+                '**Volume ({}) is already encrypted'
+                .format(original_root_volume.id))
+
+    """ Step 1: Prepare instance """
+    print('---Preparing instance')
+    # Save original mappings to persist to new volume
+    original_mappings = {}
+    original_mappings['DeleteOnTermination'] = instance.block_device_mappings[0]['Ebs']['DeleteOnTermination']
+    
+    """
+    Instance State
+	- 0 : pending
+	- 16 : running
+	- 32 : shutting-down
+	- 48 : terminated
+	- 64 : stopping
+	- 80 : stopped
+    """   
+ 
+    # Exit if instance is pending, shutting-down, or terminated
+    instance_exit_states = [0, 32, 48]
+    if instance.state['Code'] in instance_exit_states:
+        sys.exit(
+        'ERROR: Instance is {} please make sure this instance is active.'
+        .format(instance.state['Name'])
+        )
+ 
+    # Validate successful shutdown if it is running or stopping
+    if instance.state['Code'] is 16:
+        instance.stop()
+
+    try:
+        waiter_instance_stopped.wait(
+            InstanceIds=[
+                instance_id,
+            ]
+        )
+    except botocore.exceptions.WaiterError as e:
+        sys.exit('ERROR: {}'.format(e))
+
+    # Set the max_attempts for this waiter (default 40) waiter_instance_stopped.config.max_attempts = 40
+
+    """ Step 2: Take snapshot of volume """
+    print('---Create snapshot of volume ({})'.format(original_root_volume.id))
+    snapshot = ec2.create_snapshot(
+               VolumeId=original_root_volume.id,
+               Description='Snapshot of volume ({})'.format(original_root_volume.id),
+               )
+ 
+    try:
+        waiter_snapshot_complete.wait(
+            SnapshotIds=[
+                snapshot.id,
+            ]
+        )
+    except botocore.exceptions.WaiterError as e:
+        # Clean up the snapshot to reduce clutter (optional)
+        snapshot.delete()
+        sys.exit('ERROR: {}'.format(e))
+
+    """ Step 3: Create encrypted volume """
+    print('---Create encrypted copy of snapshot')
+    if customer_master_key:
+        # Use custom key
+        snapshot_encrypted_dict = snapshot.copy(
+            SourceRegion=session.region_name,
+            Description='Encrypted copy of snapshot #{}'
+            .format(snapshot.id),
+            KmsKeyId=customer_master_key,
+            Encrypted=True,
+            )
+    else:
+        # Use default key
+        snapshot_encrypted_dict = snapshot.copy(
+            SourceRegion=session.region_name,
+            Description='Encrypted copy of snapshot ({})'
+            .format(snapshot.id),
+            Encrypted=True,
+        )
+ 
+    snapshot_encrypted = ec2.Snapshot(snapshot_encrypted_dict['SnapshotId'])
+ 
+    try:
+        waiter_snapshot_complete.wait(
+            SnapshotIds=[
+                snapshot_encrypted.id,
+            ],
+        )
+    except botocore.exceptions.WaiterError as e:
+        snapshot.delete()
+        snapshot_encrypted.delete()
+        sys.exit('ERROR: {}'.format(e))
+
+    print('---Create encrypted volume from snapshot')
+    volume_encrypted = ec2.create_volume(
+        SnapshotId=snapshot_encrypted.id,
+        AvailabilityZone=instance.placement['AvailabilityZone']
+    )
+
+    """ Step 4: Detach current root volume """
+    print('---Deatch volume {}'.format(original_root_volume.id))
+    instance.detach_volume(
+        VolumeId=original_root_volume.id,
+        Device=instance.root_device_name,
+    )
+
+    """ Step 5: Attach current root volume """
+    print('---Attach volume {}'.format(volume_encrypted.id))
+    try:
+        waiter_volume_available.wait(
+            VolumeIds=[
+                volume_encrypted.id,
+            ],
+        )
+    except botocore.exceptions.WaiterError as e:
+        snapshot.delete()
+        snapshot_encrypted.delete()
+        volume_encrypted.delete()
+        sys.exit('ERROR: {}'.format(e))
+ 
+    instance.attach_volume(
+        VolumeId=volume_encrypted.id,
+        Device=instance.root_device_name
+    )
+
+    """ Step 6: Restart instance """
+    # Modify instance attributes
+    instance.modify_attribute(
+        BlockDeviceMappings=[
+        {
+            'DeviceName': instance.root_device_name,
+            'Ebs': {
+                'DeleteOnTermination':
+                original_mappings['DeleteOnTermination'],
+            },
+        },
+        ],
+    )
+    print('---Restart instance')
+    instance.start()
+ 
+    try:
+        waiter_instance_running.wait(
+            InstanceIds=[
+                instance_id,
+            ]
+        )
+    except botocore.exceptions.WaiterError as e:
+        sys.exit('ERROR: {}'.format(e))
+
+    """ Step 7: Clean up """
+    """
+    print('---Clean up resources')
+    # Delete snapshots and original volume
+    snapshot.delete()
+    snapshot_encrypted.delete()
+    original_root_volume.delete()
+    """
+
+if __name__ == '__main__':
+    main(sys.argv)
